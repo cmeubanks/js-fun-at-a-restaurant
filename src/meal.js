eb80af2d3d4a95ec4089e407117b8ea097515580
Caroline Eubanks <cmeubanks@gmail.com>
2021-02-04 09:06:08 -0700

    first 8 test suites pass

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,26 @@
-function nameMenuItem() {
+function nameMenuItem(food) {
+ return `Delicious ${food}`;
+};
 
-}
+function createMenuItem(menuItemName, price, food) {
+  var menuItem = {
+  name: menuItemName,
+  price: price,
+  type: food,
+  }
+  return menuItem
+};
 
+function addIngredients(food, array) {
+   array.push(food)
+   return array
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
