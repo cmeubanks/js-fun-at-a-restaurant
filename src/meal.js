abaa5bb17669ce3a4eed284aa7f68528cc27aeb5
Caroline Eubanks <cmeubanks@gmail.com>
2021-02-04 11:46:09 -0700

    completed meal-test.js tests

--- src/meal.js
+++ src/meal.js
@@ -11,16 +11,48 @@ function createMenuItem(menuItemName, price, food) {
   return menuItem
 };
 
+// function addIngredients(food, array) {
+//   for(var i = 0; i < array.length; i++) {
+//     if(array[i] === food) {
+//       return false
+//     } else {
+//       array.push(food)
+//     }
+//   }
+// };
+
 function addIngredients(food, array) {
-   array.push(food)
+   if(array.includes(food)) {
+     return false
+   } else {
+     array.push(food)
+   }
    return array
 };
+
+function formatPrice(objectPrice) {
+ return `$${objectPrice}`
+};
+
+function decreasePrice(objectPrice) {
+  var newPrice = (objectPrice - .6)
+  return newPrice
+};
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
