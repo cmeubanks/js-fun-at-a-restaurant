1ba9201cadf20f48008e612808e411109a7327df
Caroline Eubanks <cmeubanks@gmail.com>
2021-02-06 11:28:30 -0700

    completed restaurant-test.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -17,13 +17,22 @@ function addMenuItem(restaurant, mealType) {
 
   if(mealType.type === "lunch") {
     restaurant.menus.lunch.push(mealType);
-  } else {
+  } else if (mealType.type === "breakfast") {
   restaurant.menus.breakfast.push(mealType);
+  } else {
+    restaurant.menus.dinner.push(mealType);
   }
 };
 
-function removeMenuItem() {
-  
+function removeMenuItem(restaurant, mealName, mealType) {
+  var restaurantArray = restaurant.menus[mealType]
+  for(var i = 0; i < restaurantArray.length; i++) {
+    if(restaurantArray[i].name === mealName) {
+      restaurantArray.splice(i, 1)
+      return `No one is eating our ${mealName} - it has been removed from the ${mealType} menu!`;
+    }
+  }
+  return `Sorry, we don't sell ${mealName}, try adding a new recipe!`;
 }
 
 
